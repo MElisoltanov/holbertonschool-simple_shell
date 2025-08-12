26a723ae05dae2e19f850b1a47bcc07c571ec233
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-08-12 09:13:09 +0200

    Creation files for Github

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#endif
