802c44255a0c5aa6a3099bb8b0ad561521ba0d1a
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-08-15 17:46:13 +0200

    Add builtin.c: implement built-in command handlers

---
+++ builtin.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+/**
+ * is_builtin - Checks if the command
+ * is a recognized built-in
+ * @cmd: command to test
+ *
+ * Return: 1 if builtin, or 0
+ */
+int is_builtin(const char *cmd)
+{
+	if (cmd == NULL || cmd[0] == '\0')
+		return (0);
+
+	if (strcmp(cmd, "exit") == 0)
+		return (1);
+
+	if (strcmp(cmd, "env") == 0)
+		return (1);
+
+	return (0);
+}
+/**
+ * run_builtin - Executes corresponding builtin
+ * @argv: arguments (argv[0] = commande)
+ * @exit_shell: pointer to shell shutdown flag
+ * @exit_status: pointer to exit code
+ *
+ * Return: builtin status code
+ */
+int run_builtin(char **argv, int *exit_shell, int *exit_status)
+{
+	if (argv == NULL || argv[0] == NULL)
+		return (0);
+
+	if (strcmp(argv[0], "exit") == 0)
+		return (bi_exit(argv, exit_shell, exit_status));
+
+	else if (strcmp(argv[0], "env") == 0)
+		return (bi_env(argv, exit_status));
+
+	return (0);
+}
