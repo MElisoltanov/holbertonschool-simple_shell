2eb8f77eca43d48075f8d11761cbcc7de4dcb57d
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-08-16 13:11:05 +0200

    Fix execute_command to avoid forking if command does not exist and return correct exit status

--- execute.c
+++ execute.c
@@ -14,12 +14,29 @@
  * Return: Exit status code of the command.
  *
  */
+#include "shell.h"
+
+/**
+ * execute_command - Executes a command (builtin or external).
+ *
+ * @argv: Null-terminated array of command arguments.
+ * @exit_shell: Pointer to int that indicates if the shell should exit.
+ * @exit_status: Pointer to int that stores the exit status of the command.
+ * @prog_name: Name of the shell program (used for error messages).
+ * @line_no: Line number of the command (used for error messages).
+ *
+ * Return: Always 0.
+ */
 int execute_command(char **argv, int *exit_shell, int *exit_status,
 char *prog_name, int line_no)
 {
 	pid_t pid;
 	int status;
 	char *path;
+	struct stat st;
+
+	if (argv == NULL || argv[0] == NULL)
+		return (0);
 
 	if (is_builtin(argv[0]))
 	{
@@ -28,40 +45,39 @@ char *prog_name, int line_no)
 	}
 	if (strchr(argv[0], '/') != NULL)
 	{
-		pid = launch_process(argv[0], argv, environ, &status);
-		if (pid < 0)
+		if (access(argv[0], X_OK) != 0 || stat(argv[0], &st) != 0 || !S_ISREG(st.st_mode))
 		{
-			perror("fork failed");
-			*exit_status = 1;
+			fprintf(stderr, "%s: %d: %s: not found\n",
+				prog_name, line_no, argv[0]);
+			*exit_status = 127;
 			return (0);
 		}
-		if (WIFEXITED(status))
-			*exit_status = WEXITSTATUS(status);
-		else
-			*exit_status = 1;
+		pid = launch_process(argv[0], argv, environ, &status);
 	}
 	else
 	{
 		path = resolve_path(argv[0]);
 		if (path == NULL)
 		{
-			fprintf(stderr, "%s: %d: %s: not found\n", prog_name, line_no, argv[0]);
+			fprintf(stderr, "%s: %d: %s: not found\n",
+				prog_name, line_no, argv[0]);
 			*exit_status = 127;
 			return (0);
 		}
 		pid = launch_process(path, argv, environ, &status);
 		free(path);
-		if (pid < 0)
-		{
-			perror("fork failed");
-			*exit_status = 1;
-			return (0);
-		}
-		if (WIFEXITED(status))
-			*exit_status = WEXITSTATUS(status);
-		else
-			*exit_status = 1;
 	}
+	if (pid < 0)
+	{
+		perror("fork failed");
+		*exit_status = 1;
+		return (0);
+	}
+	if (WIFEXITED(status))
+		*exit_status = WEXITSTATUS(status);
+	else
+		*exit_status = 1;
+
 	return (0);
 }
 
